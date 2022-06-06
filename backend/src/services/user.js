dbe41454cdd6126f7f42a0e7fa41286b96bb0ea6
20181101remon <mindy80230@gmail.com>
2022-06-06 16:25:58 +0800

    匿名登入

--- backend/src/services/user.js
+++ backend/src/services/user.js
@@ -2,7 +2,7 @@
  * @Author: 20181101remon mindy80230@gmail.com
  * @Date: 2022-05-16 14:33:57
  * @LastEditors: 20181101remon mindy80230@gmail.com
- * @LastEditTime: 2022-05-18 17:01:19
+ * @LastEditTime: 2022-06-06 16:18:12
  * @FilePath: \backend\src\services\user.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -19,6 +19,32 @@ const getAllUserInfo = async (mail) => {
   });
 
   return users;
+};
+
+const getTouristInfo = async (username) => {
+  
+  let users = await db["USERS"].findOne({
+    where: {
+      username: username,
+    },
+    attributes: ["username","id"],
+  });
+
+  return users;
+};
+
+
+const creatTourist = async (userData) => {
+  await  db["USERS"]
+    .create({ username: userData})
+    .then((result) => {
+  
+     return result; // 成功回傳result結果
+    })
+    .catch((err) => {
+      return err;
+    });
+ 
 };
 
 const creatUser = async (userData) => {
@@ -75,5 +101,7 @@ module.exports = {
   creatUser,
   checkMailExistOrNot,
   checkUserExistOrNot,
-  addToken
+  addToken,
+  creatTourist,
+  getTouristInfo
 };
